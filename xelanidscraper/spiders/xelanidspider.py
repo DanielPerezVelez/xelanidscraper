117613346f05fe22bb9be1079e71ea754ecb7ed2
DanielPerezVelez <l18530435@cancun.tecnm.mx>
2023-08-11 22:36:53 -0500

    first commit

---
+++ xelanidscraper/spiders/xelanidspider.py
@@ -0,0 +1,16 @@
+import scrapy
+
+class XelanidSpider(scrapy.Spider):
+    name = 'xelanid'
+    start_urls = ['https://www.airbnb.mx/rooms/715825575916402712']
+
+    def parse(self, response): 
+        for image1 in response.css('div._13sj9hk'):
+            try:
+                yield {
+                    'image': image1.css('img.itu7ddv.i1mla2as.i1cqnm0r.dir.dir-ltr').attrib['src']
+                }
+            except:
+                yield {
+                    'image': 'no image :('
+                }
